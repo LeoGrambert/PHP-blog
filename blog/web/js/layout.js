6417fcbf8036c27b5099a3e2041873948f3e3bb4
LeoGrambert <leogrambert@gmail.com>
2017-03-29 17:28:31 +0200

    I worked on blog design. I used a materialize template

---
+++ blog/web/js/layout.js
@@ -0,0 +1,16 @@
+/**
+ * Created by leo on 29/03/17.
+ */
+Array.prototype.forEach.call(document.querySelectorAll('.mdl-card__media'), function(el) {
+    var link = el.querySelector('a');
+    if(!link) {
+        return;
+    }
+    var target = link.getAttribute('href');
+    if(!target) {
+        return;
+    }
+    el.addEventListener('click', function() {
+        location.href = target;
+    });
+});
