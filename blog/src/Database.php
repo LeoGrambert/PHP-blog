83efaf95ab45df88538cb4d0069714c1979bb07c
LeoGrambert <leogrambert@gmail.com>
2017-03-30 10:33:21 +0200

    I fixed an encoding problem in BDD

--- blog/src/Database.php
+++ blog/src/Database.php
@@ -37,7 +37,7 @@ class Database
      */
     private function getPDO(){
         if($this->pdo === null){
-            $pdo = new PDO('mysql:dbname='.$this->db_name.';host='.$this->db_host, $this->db_user, $this->db_pass);
+            $pdo = new PDO('mysql:dbname='.$this->db_name.';host='.$this->db_host, $this->db_user, $this->db_pass, [PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8']);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $this->pdo = $pdo;
         }
