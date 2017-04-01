4de9443c50d2330ae8e0947cb8d412c5efbde130
LeoGrambert <leogrambert@gmail.com>
2017-04-01 19:11:32 +0200

    I added the article sharing feature

---
+++ blog/web/js/article.js
@@ -0,0 +1,15 @@
+/**
+ * Created by leo on 01/04/17.
+ */
+$(function(){
+
+    $('.share').click(function() {
+        var $url = document.location.href;
+        var $html = "<div class='shareWindow'><img id='crossShare' src='../../web/img/cross.png' alt='cross_to_close'>Copiez le lien ci-dessous pour partager l'article<input type='text' value='"+$url+"'></div>";
+        $('.share').after($html);
+        $('#crossShare').click(function () {
+            $('.shareWindow').remove();
+        })
+    });
+    
+});
