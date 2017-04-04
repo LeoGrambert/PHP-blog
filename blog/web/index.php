c91127e352fc62bf453665b31f44edc6178bf142
LeoGrambert <leogrambert@gmail.com>
2017-04-04 11:36:55 +0200

    Refactoring code : add a routing file and a controller.

--- blog/web/index.php
+++ blog/web/index.php
@@ -6,8 +6,8 @@
  */
 
 use app\App;
-use src\Table\Article;
-use src\Table\Comment;
+use app\Router;
+
 
 /*
  * Load what we need
@@ -16,69 +16,12 @@ use src\Table\Comment;
 //call load app function -> initialize session and load autoloader
 require '../app/App.php';
 App::load();
-//load twig
-$twig = App::twig();
-//load class
-$articleClass = new Article();
-$commentClass = new Comment();
-
-
-//query to get all articles
-$articles = $articleClass->getArticles();
 
 
 /*
  * Router
  *
  */
-// route the request internally
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
-//If homepage
-if ('/web/index.php' === $uri || '/web/' === $uri) {
-    echo $twig->render('home.html.twig', ['articles'=>$articles]);
-}
-
-//If article page
-elseif ('/web/index.php/article' === $uri && isset($_GET['id'])) {
-    //query to get article by id
-    $article = $articleClass->getArticleById();
-
-    //query to add a comment
-    if(isset($_POST['content']) && !empty($_POST['content']))
-    {
-        $addAComment = $commentClass->addComment();
-    } else {
-    }
-
-    //query to get all comments by article id
-    $comments = $commentClass->getComments();
-    //Get comments replies
-    $comments_by_id = [];
-    foreach($comments as $comment){
-        $comments_by_id[$comment->getId()] = $comment;
-    }
-    foreach ($comments as $k => $comment){
-        //If comment has children, we add them in an array, and we call this array in twig view with a margin-left
-        if ($comment->getParentCommentId() != 0){
-            $comments_by_id[$comment->getParentCommentId()]->children[] = $comment;
-            unset($comments[$k]);
-        }
-    }
-
-    echo $twig->render('article.html.twig', [
-        'article'=>$article,
-        'comments'=>$comments
-    ]);
-
-//If admin page
-} elseif ('/web/index.php/admin' === $uri){
-    echo $twig->render('admin.html.twig');
-
-//If not => 404
-} else {
-    header('HTTP/1.1 404 Not Found');
-    echo $twig->render('error404.html.twig');
-}
-
-
+require '../app/Router.php';
+$router = new Router();
+$router->routerRequete();
